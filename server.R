6ed7c73805ff5d70ca8d19f6acc331aaffa90e54
ZachG88 <zgen88@uw.edu>
2023-03-08 11:28:50 -0800

    Zach added elements

--- server.R
+++ server.R
@@ -7,7 +7,7 @@ library("maps")
 library("dplyr")
 library("zipcodeR")
 
-# Load in relevant data
+# load in data
 full_df <- read.csv("fulldataframe.csv")
 
 # Audreys work
@@ -15,57 +15,48 @@ full_df <- read.csv("fulldataframe.csv")
 # Noors work
 
 # Zachs work
+certs <- full_df %>%  select(Vendor_Formal_Name, certification, Date_Of_Establishment) %>% 
+  filter(Date_Of_Establishment != "") %>% separate_rows(certification, Date_Of_Establishment, sep = ",")
+
+certs$Date_Of_Establishment <- format(as.Date(certs$Date_Of_Establishment), "%Y")
+
+certs <- certs %>% group_by(certification, Date_Of_Establishment) %>% summarise(certs_per_year = n()) %>% 
+  mutate(full_date_estab = as.Date(paste0(Date_Of_Establishment,"-1-1")))
+
+cert_types <- unique(certs$certification)
 
 
 # renders - please keep data in this section to a minimum
 server <- function(input, output) {
   
   # Audreys render
-  # output$zipcode_map <- renderPlotly({
-  #   
-  #   
-  #   return(zipcode_map)
-  # })
+  output$zipcode_map <- renderPlotly({
+    
+    
+    return(zipcode_map)
+  })
   
   # Noors render
   
-  output$ethnicity_plot <- renderPlotly({
+  output$NAME_HERE <- renderPlotly({
     
-    # Filter for relevant data
-    selected_df <- full_df %>% 
-      filter(Ethnicity != "N/A") %>% 
-      filter(City %in% input$city_selection) %>% 
-      group_by(Ethnicity, State) %>% 
-      summarise(num_vendors = n_distinct(Vendor_Formal_Name, na.rm = T))
     
-    # Create bar plot
-    ethnicity_plot <- ggplot(data = selected_df) +
-      geom_col(mapping = aes(
-        x = num_vendors,
-        y = reorder(Ethnicity, +num_vendors),
-        fill = Ethnicity,
-        text = paste("Number of Vendors:", num_vendors, '\n', "State:", State)
-      )) +
-      scale_fill_manual(
-        values =
-          c("#bc6c25", "#606c38", "#dda15e", "#283618")
-      ) +
-      labs(
-        title = paste("Representation of Ethnicities Among", input$city_selection, "Businesses"),
-        x = "Number of Vendors",
-        y = "Ethnicity"
-      ) +
-      theme(panel.background = element_rect(fill = "#f5ebe0"))
-    
-    return(ggplotly(ethnicity_plot, tooltip = "text"))
+    return(NAME_HERE)
   })
   
   # Zachs render
   
-  # output$NAME_HERE <- renderPlotly({
-  #   
-  #   
-  #   return(NAME_HERE)
-  # })
+  output$cert_plot <- renderPlotly({
+    
+    filtered_df <- certs %>%
+      filter(certification %in% input$cert_selection) %>%
+      filter(Year > input$year_selection[1] & Year < input$year_selection[2])
+    
+    cert_plot <- ggplot(filtered_df, aes(full_date_estab,certs_per_year, color = 'Certification')) +
+      geom_point()+
+      labs(title = "", x = "Year", y = "Certifications")
+    
+    return(cert_plot)
+  })
   
 }
--- ui.R
+++ ui.R
@@ -7,98 +7,103 @@ library("tidyverse")
 
 # load any data needed here (for widget choices)
 
-# load in data
-full_df <- read.csv("fulldataframe.csv")
-
 # BootSwatch Theme setup (if anyone would like to switch themes/make a custom one, go ahead!)
-# my_theme <- bs_theme_update(my_theme, bootswatch = "cosmo")
+my_theme <- bs_theme_update(my_theme, bootswatch = "cosmo")
 
 
 # Audreys tab
 
-# zipcode_viz_tab <- tabPanel(
-#   "Zipcode Map",
-#   sidebarLayout(
-#     sidebarPanel(
-#     ),
-#     mainPanel(
-#     )
-#   )
-# )
-
-# Noors tab
-
-select_widget <- selectInput(
-  inputId = "city_selection",
-  label = "Cities",
-  choices = unique(full_df$City),
-  selected = "New York",
-  multiple = F
-)
-
-bar_plot <- plotlyOutput(outputId = "ethnicity_plot")
-
-heading <- h4("Description")
-plot_description <- paste("This chart displays vendor information in an attempt to answer the question: Which ethnicities are more represented/are underrepresented among businesses in certain cities in New York? I chose to represent ethnicity data in the form of a bar plot where a user can select their desired city. This provides users with a clear and easy-to-interpret visualization of the distribution of ethnicities among various cities in the state of New York. By displaying the number of vendors for each ethnicity, my plot provides insights into the diversity of NY businesses and highlights disparities and imbalances among their representation. This information can allow us to identify which cities need more attention and resources allocated to allow minority and women-owned businesses to thrive.")
-
-ethnicity_viz_tab <- tabPanel(
-  "Ethnicity Analysis",
+zipcode_viz_tab <- tabPanel(
+  "Zipcode Map",
   sidebarLayout(
     sidebarPanel(
-      select_widget
     ),
     mainPanel(
-      bar_plot,
-      heading,
-      plot_description
     )
   )
 )
+
+# Noors tab
+
+ethnicity_viz_tab <- tabPanel(
+  "TAB NAME",
+  sidebarLayout(
+    sidebarPanel(
+    ),
+    mainPanel(
+    )
+  )
+)
+
 # Zachs tab
 
-# time_vis_tab <- tabPanel(
-#   "TAB NAME",
-#   sidebarLayout(
-#     sidebarPanel(
-#     ),
-#     mainPanel(
-#     )
-#   )
-# )
+select_widget <-
+  selectInput(
+    inputId = "cert_selection",
+    label = "Certification Type",
+    choices = cert_types,
+    selectize = TRUE,
+    # True allows you to select multiple choices...
+    multiple = TRUE,
+    selected = 
+  )
+
+slider_widget <- sliderInput(
+  inputId = "year_selection",
+  label = "year",
+  min = min(certs$full_date_estab),
+  max = max(certs$full_date_estab),
+  value = c(1915, 2015),
+  sep = "")
+
+# Put a plot in the middle of the page
+main_panel_plot <- mainPanel(
+  # Make plot interactive
+  plotlyOutput(outputId = "cert_plot")
+)
+time_vis_tab <- tabPanel(
+  "Certifications over time",
+  sidebarLayout(
+    sidebarPanel(
+      select_widget,
+      slider_widget
+    ),
+    mainPanel
+  )
+)
+
 
 # Williams tabs (partially commented out as the files dont exist yet)
 
-# intro_tab <- tabPanel(
-#   "Introduction",
-#   fluidPage(
-#     column(
-#     # includeMarkdown("intro.md")
-#     ),
-#     column(
-#     # image here?
-#     )
-#   )
-# )
+intro_tab <- tabPanel(
+  "Introduction",
+  fluidPage(
+    column(
+    # includeMarkdown("intro.md")
+    ),
+    column(
+    # image here?
+    )
+  )
+)
 
-# conclusion_tab <- tabPanel(
-#   "Conclusion",
-#   fluidPage(
-#     # includeMarkdown("conclusion.md")
-#   )
-# )
+conclusion_tab <- tabPanel(
+  "Conclusion",
+  fluidPage(
+    # includeMarkdown("conclusion.md")
+  )
+)
 
 
 # ui creation
 ui <- navbarPage(
   # Select Theme
-  # theme = my_theme,
-
+  theme = my_theme,
   # Home page title
   "Home",
   intro_tab,
   zipcode_viz_tab,
   ethnicity_viz_tab,
   time_vis_tab,
-  conclusion_tab,
-  theme = bs_theme(bootswatch = "minty")
+  conclusion_tab
 )
